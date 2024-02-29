8253fb8748644d741c4ff9d5a37aac28b3ce0641
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-02-29 21:42:14 +0500

    Refactor Services.

--- PhoneBookConsole/Services/PhoneBookService.cs
+++ PhoneBookConsole/Services/PhoneBookService.cs
@@ -1,53 +1,94 @@
-﻿using PhoneBookConsole.Models;
+﻿using PhoneBookConsole.Brokers;
+using PhoneBookConsole.Brokers.Interfaces;
+using PhoneBookConsole.Models;
 using System;
 
 namespace PhoneBookConsole.Services
 {
     class PhoneBookService
     {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IFileBroker fileBroker;
         private const int MaxContacts = 100;
         private Contact[] contacts { get; set; } = new Contact[MaxContacts];
         private int contactCount = 0;
+
+        public PhoneBookService()
+        {
+            loggingBroker = new LoggingBroker();
+            fileBroker = new FileBroker();
+            ReadContactsFromFile();
+        }
+
+        private void ReadContactsFromFile()
+        {
+            string[] lines = fileBroker.ReadDataFromFile();
+
+            foreach (string line in lines)
+            {
+                string[] contactInfo = line.Split(' ');
+
+                if (contactInfo.Length == 3)
+                {
+                    Contact contact = new Contact
+                    {
+                        Id = contactInfo[0],
+                        Name = contactInfo[1],
+                        Number = contactInfo[2]
+                    };
+
+                    contacts[contactCount] = contact;
+                    contactCount++;
+                }
+            }
+        }
+
         private void DisplayContactDetails(Contact contact)
         {
-            Console.WriteLine($"Contact : {contact.Name} , {contact.Number}");
+            loggingBroker.Log($"Contact : {contact.Name} , {contact.Number}");
         }
         private void DisplayContactDetails(Contact[] contacts)
         {
             for (int iteration = 0; iteration < contactCount; iteration++)
             {
-                DisplayContactDetails(contacts[iteration]);
+                if (contacts[iteration] is not null)
+                {
+                    DisplayContactDetails(contacts[iteration]);
+                }
             }
         }
         
-        public void DisplayContactById(int Id)
+        public void DisplayContactById(string id)
         {
-            var contact = contacts[Id++];
+            var contact = contacts;
+
             if (contact == null)
             {
-                Console.WriteLine("Contact not found");
+                loggingBroker.LogError("Contact is not found");
             }
             else
             {
                 DisplayContactDetails(contact);
             }
         }
+
         public void DisplayAllContact()
         {
             DisplayContactDetails(contacts);
         }
+
         public void DisplayMatchingContacts(string searchPharse)
         {
             Contact[] foundContacts = Array.FindAll(contacts, c => c.Name.Contains(searchPharse));
 
             if (foundContacts.Length > 0)
             {
-                Console.WriteLine("Matching contacts:");
+                loggingBroker.LogInformation("Matching contacts:");
                 DisplayContactDetails(foundContacts);
             }
             else
             {
-                Console.WriteLine("No matching contacts found.");
+                loggingBroker.LogInformation("No matching contacts found.");
             }
         }
 
@@ -55,13 +96,13 @@ namespace PhoneBookConsole.Services
         {
             if (contactCount < MaxContacts)
             {
-                contacts[contactCount] = new Contact(contact.Name, contact.Number);
+                fileBroker.InsertDataToFile($"{contact.Id} {contact.Name} {contact.Number}");
                 contactCount++;
-                Console.WriteLine("Contact added successfully.");
+                loggingBroker.LogInformation("Contact added successfully.");
             }
             else
             {
-                Console.WriteLine("Phonebook is full. Cannot add more contacts.");
+                loggingBroker.LogInformation("Phonebook is full. Cannot add more contacts.");
             }
         }
 
@@ -95,7 +136,6 @@ namespace PhoneBookConsole.Services
                 Console.WriteLine("Invalid contact ID.");
             }
         }
-
     }
 }
 
