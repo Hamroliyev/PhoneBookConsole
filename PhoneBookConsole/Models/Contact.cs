839ae293e39db2fc35ce9839f92ef2a403a71ee0
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-03-05 21:03:38 +0500

    Add Contact.

--- PhoneBookConsole/Models/Contact.cs
+++ PhoneBookConsole/Models/Contact.cs
@@ -1,9 +1,9 @@
 ﻿namespace PhoneBookConsole.Models
 {
-    class Contact
+    public class Contact
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Number { get; set; }
+        public string Phone { get; set; }
     }
 }
---
+++ PhoneBookConsole/Services/Contacts/ContactService.cs
@@ -0,0 +1,22 @@
+﻿using PhoneBookConsole.Brokers.Loggings;
+using PhoneBookConsole.Brokers.Storages;
+using PhoneBookConsole.Models;
+
+namespace PhoneBookConsole.Services.Contacts
+{
+    public class ContactService : IContactService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ContactService()
+        {
+            this.storageBroker = new FileStorageBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+        public Contact AddContact(Contact contact)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
